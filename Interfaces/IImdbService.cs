be32f7b1708d5a903cf949936e0e4cdee15d26f1
carlos-palalo <palaxcarlos@gmail.com>
2021-05-21 17:46:31 +0200

    Update GenerateBBDD

--- Interfaces/IImdbService.cs
+++ Interfaces/IImdbService.cs
@@ -7,9 +7,5 @@ namespace API_ISDb.Interfaces
     {
         bool GenerateBBDD();
         string Request(string url);
-        void WriteActor(List<RequestSerie.Actor> items, Serie ser, string tipo);
-        bool WriteBBDD(string infoserie);
-        void WriteGenero(List<RequestSerie.GenreList> items, Serie ser);
-        void WriteItem(List<RequestSerie.Item> items, Serie ser, string tipo);
     }
 }
--- Services/ImdbService.cs
+++ Services/ImdbService.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -93,11 +94,132 @@ namespace API_ISDb.Services
             string url = "";
             int cont = 0;   //MAX 100 PER DAY
             url = urlBase + urlTop;
-            string top250 = Request(url);
 
-            RequestSeries.Root responseTop = JsonConvert.DeserializeObject<RequestSeries.Root>(top250);
+            RequestSeries.Root responseTop250 = JsonConvert.DeserializeObject<RequestSeries.Root>(Request(url));
 
-            foreach (Item item in responseTop.items)
+            foreach (Item serie in responseTop250.items)
+            {
+                if (cont >= 1)
+                    return true;
+
+                if (_serie.Search(serie.title) == null)
+                {
+                    url = urlBase + urlSerie + serie.id + auxUrlSerie;
+                    RequestSerie.Root responseSerie = JsonConvert.DeserializeObject<RequestSerie.Root>(Request(url));
+                    Console.WriteLine(responseSerie.title);
+
+                    //Inserto la serie
+                    var infoSerie = new Serie
+                    {
+                        Titulo = responseSerie.title,
+                        Poster = responseSerie.image,
+                        Year = Int32.Parse(responseSerie.year),
+                        Trailer = responseSerie.trailer.linkEmbed,
+                        Sinopsis = responseSerie.plot,
+                        SerieGenero = new Collection<SerieGenero>
+                        {
+                            
+                        }
+                    };
+                    _context.Add(infoSerie);
+                    _context.SaveChanges();
+
+                    //Obtengo id de la serie recien insertada
+                    var idSerie = _context.Serie.FirstOrDefault(x => x.Titulo == responseSerie.title).IdSerie;
+
+                    /******************************************************************************/
+
+                    //Lista de nombres de generos en la DB
+                    var listDbG = _context.Genero
+                        .Select(x => x.Nombre)
+                        .ToList();
+
+                    //Lista de nombres de generos de la API externa
+                    var listG = responseSerie.genreList
+                        .Select(x => x.value)
+                        .ToList();
+
+                    //Compruebo si existen los generos de la consulta en la DB y selecciono sus ID
+                    var gExist = _context.Genero
+                        .Where(x => listG.Contains(x.Nombre))
+                        .Select(x => x.IdGenero)
+                        .ToList();
+
+                    //Añado filas a SerieGenero en la DB con el idSerie y los ID de gExist
+                    List<SerieGenero> sg = new List<SerieGenero>();
+                    foreach (var id in gExist)
+                    {
+                        sg.Add(new SerieGenero { GeneroIdGenero = id, SerieIdSerie = idSerie });
+                    }
+                    _context.SerieGenero.AddRange(sg);
+
+                    //Selecciono los generos que no están en la Db
+                    var gNExist = responseSerie.genreList
+                        .Where(x => !listDbG.Contains(x.value))
+                        .Select(x => x.value)
+                        .ToList();
+
+                    //Añado los generos nuevos
+                    List<Genero> g = new List<Genero>();
+                    foreach (var name in gNExist)
+                    {
+                        g.Add(new Genero { Nombre = name });
+                    }
+                    _context.Genero.AddRange(g);
+
+                    //Obtengo los ids de los generos recientemente añadidos
+                    var idListG = _context.Genero
+                        .Where(x => gNExist.Contains(x.Nombre))
+                        .Select(x => x.IdGenero)
+                        .ToList();
+
+                    //Añado filas a SerieGenero con el idSerie y los ID de los generos nuevos
+                    List<SerieGenero> sgnew = new List<SerieGenero>();
+                    foreach (var id in idListG)
+                    {
+                        sgnew.Add(new SerieGenero { GeneroIdGenero = id, SerieIdSerie = idSerie });
+                    }
+                    _context.SerieGenero.AddRange(sgnew);
+
+                    /******************************************************************************/
+
+                    /******************************************************************************/
+
+                    //Lista de nombres de la tabla Reparto
+                    var listDbR = _context.Reparto.Select(x => x.Name).ToList();
+
+                    //Lista de nombres de directores de la consulta
+                    var listaRDir = responseSerie.fullCast.directors.items.Select(x => x.name);
+
+                    //Lista de nombres de directores de la consulta
+                    var listaRWrt = responseSerie.fullCast.writers.items.Select(x => x.name);
+
+                    //Lista de nombres de directores de la consulta
+                    var listaRAct = responseSerie.fullCast.actors.Select(x => x.name);
+
+                    //Lista ID de directores en Db
+                    var idRDirExist = _context.Reparto
+                        .Where(x => listaRDir.Contains(x.Name))
+                        .Select(x=>x.IdReparto).ToList();
+
+                    //Lista ID de escritores en Db
+                    var idRWrtExist = _context.Reparto
+                        .Where(x => listaRWrt.Contains(x.Name))
+                        .Select(x => x.IdReparto).ToList();
+
+                    //Lista ID de actores en Db
+                    var idRActExist = _context.Reparto
+                        .Where(x => listaRAct.Contains(x.Name))
+                        .Select(x => x.IdReparto).ToList();
+
+                    /******************************************************************************/
+                    cont++;
+
+                };
+            }
+
+            /*
+            foreach (Item item in responseTop250.items)
             {
                 if (cont == 5)
                     return true;
@@ -105,7 +227,6 @@ namespace API_ISDb.Services
                 Serie ser = _serie.Search(item.title);
                 if (ser == null)
                 {
-                    url = urlBase + urlSerie + item.id + auxUrlSerie;
                     //Console.WriteLine(item.id);
                     string info = Request(url);
                     cont++;
@@ -118,7 +239,7 @@ namespace API_ISDb.Services
 
                     }
                 }
-            }
+            }*/
 
             return true;
         }
@@ -128,7 +249,7 @@ namespace API_ISDb.Services
         /// </summary>
         /// <param name="infoserie"></param>
         /// <returns></returns>
-        public Boolean WriteBBDD(string infoserie)
+        private Boolean WriteBBDD(string infoserie)
         {
             //Console.WriteLine(infoserie);
             RequestSerie.Root obj = JsonConvert.DeserializeObject<RequestSerie.Root>(infoserie);
@@ -164,7 +285,7 @@ namespace API_ISDb.Services
         /// <param name="items"></param>
         /// <param name="ser"></param>
         /// <param name="tipo"></param>
-        public void WriteItem(List<RequestSerie.Item> items, Serie ser, string tipo)
+        private void WriteItem(List<RequestSerie.Item> items, Serie ser, string tipo)
         {
             foreach (RequestSerie.Item item in items)
             {
@@ -193,7 +314,7 @@ namespace API_ISDb.Services
         /// <param name="items"></param>
         /// <param name="ser"></param>
         /// <param name="tipo"></param>
-        public void WriteActor(List<RequestSerie.Actor> items, Serie ser, string tipo)
+        private void WriteActor(List<RequestSerie.Actor> items, Serie ser, string tipo)
         {
             foreach (RequestSerie.Actor actor in items)
             {
@@ -221,8 +342,9 @@ namespace API_ISDb.Services
         /// </summary>
         /// <param name="items"></param>
         /// <param name="ser"></param>
-        public void WriteGenero(List<RequestSerie.GenreList> items, Serie ser)
+        private void WriteGenero(List<RequestSerie.GenreList> items, Serie ser)
         {
+
             foreach (RequestSerie.GenreList genre in items)
             {
                 Genero genero = new Genero();
